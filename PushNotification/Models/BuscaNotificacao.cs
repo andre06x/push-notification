f77abe317b9275abdcf9f9c15e4342f7c80a2842
André Silva <andre06x@gmail.com>
2024-01-22 09:58:30 -0300

    busca notificacao

---
+++ PushNotification/Models/BuscaNotificacao.cs
@@ -0,0 +1,14 @@
+﻿namespace PushNotification.Models
+{
+    public class BuscaNotificacao
+    {
+        public int? usuario_id { get; set; }
+        public int? inscricao_id { get; set; }
+        public Guid? aparelho_id { get; set; }
+
+        public string? title { get; set; }
+        public string? body { get; set; }
+        public string? icon { get; set; }
+
+    }
+}
