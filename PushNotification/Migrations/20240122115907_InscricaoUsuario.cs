564cb295aa6574384cefa7d1d8a783a912475b4d
André Silva <andre06x@gmail.com>
2024-01-22 09:57:17 -0300

    migration

---
+++ PushNotification/Migrations/20240122115907_InscricaoUsuario.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PushNotification.Migrations
+{
+    public partial class InscricaoUsuario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nome = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inscricao",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    aparelho = table.Column<Guid>(type: "uuid", nullable: false),
+                    endpoint = table.Column<string>(type: "text", nullable: false),
+                    p26dh = table.Column<string>(type: "text", nullable: false),
+                    auth = table.Column<string>(type: "text", nullable: false),
+                    usuario_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inscricao", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Inscricao_Usuario_usuario_id",
+                        column: x => x.usuario_id,
+                        principalTable: "Usuario",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inscricao_endpoint",
+                table: "Inscricao",
+                column: "endpoint",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inscricao_usuario_id",
+                table: "Inscricao",
+                column: "usuario_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inscricao");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
