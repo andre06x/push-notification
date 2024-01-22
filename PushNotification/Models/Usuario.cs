5674aa0bc3539e7856cb42341f09bce02fc1ccd9
André Silva <andre06x@gmail.com>
2024-01-22 09:59:44 -0300

    usuario model

---
+++ PushNotification/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PushNotification.Models
+{
+    public class Usuario
+    {
+        [Column("id")]
+
+        public int id { get; set; }
+
+        [Column("nome")]
+        public string? nome { get; set; }
+    }
+}
