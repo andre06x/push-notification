a99a724343a5b7eb826c2766f3bec88ae4a01b3d
André Silva <andre06x@gmail.com>
2024-01-22 09:56:57 -0300

    dto push

---
+++ PushNotification/DTO/PushTodos.cs
@@ -0,0 +1,9 @@
+﻿namespace PushNotification.DTO
+{
+    public class PushTodos
+    {
+        public string body { get; set; }
+        public string title { get; set; }
+        public string? icon { get; set; }
+    }
+}
