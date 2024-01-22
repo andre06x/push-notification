c52a68781f9e3a0f654736d5f0388a5f78c0d11e
André Silva <andre06x@gmail.com>
2024-01-22 09:59:05 -0300

    push model

---
+++ PushNotification/Models/PushNotificationModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PushNotification.Models
+{
+    public class PushNotificationModel
+    {
+        public string Endpoint { get; set; }
+        public string P256dh { get; set; }
+        public string Auth { get; set; }
+        public string body { get; set; }
+        public string title { get; set; }
+        public string? icon { get; set; }
+    }
+
+
+}
