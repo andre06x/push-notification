b94de730e41484976d1db1ac046a2f36328491d6
André Silva <andre06x@gmail.com>
2024-01-22 09:58:49 -0300

    inscricao model

---
+++ PushNotification/Models/Inscricao.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PushNotification.Models
+{
+    public class Inscricao
+    {
+        [Column("id")]
+        public int id { get; set; }
+
+        [Column("aparelho")]
+        public Guid aparelho { get; set; }
+
+        [Index(IsUnique = true)]
+        [Column("endpoint")]
+        public string endpoint { get; set; }
+
+        [Column("p26dh")]
+        public string p26dh { get; set; }
+
+        [Column("auth")]
+        public string auth { get; set; }
+
+
+        [Column("usuario_id")]
+        public int? usuario_id { get; set; }
+
+        [ForeignKey("usuario_id")]
+        public virtual Usuario? Usuario { get; set; }
+
+    }
+}
