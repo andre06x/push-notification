299dd539e5c04ad8a0f2a06f1e2abc0a945bd237
André Silva <andre06x@gmail.com>
2024-01-22 09:56:12 -0300

    usuario map

---
+++ PushNotification/Configuracao/Map/UsuarioMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using PushNotification.Models;
+
+namespace PushNotification.Configuracao.Map
+{
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(x => x.id);
+            builder.Property(x => x.id).ValueGeneratedOnAdd();
+            builder.Property(x => x.nome);
+
+
+        }
+    }
+}
