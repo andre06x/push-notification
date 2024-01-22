b96e3c8fdc788c67c849acdad798949b01bcdb90
André Silva <andre06x@gmail.com>
2024-01-22 09:55:53 -0300

    contexto

---
+++ PushNotification/Configuracao/Contexto.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PushNotification.Configuracao.Map;
+using PushNotification.Models;
+
+namespace PushNotification.Configuracao
+{
+        public class Contexto : DbContext
+        {
+            public Contexto(DbContextOptions<Contexto> options) : base(options)
+            {
+                Database.EnsureCreated();
+            }
+
+            public DbSet<Usuario> Usuario { get; set; }
+            public DbSet<Inscricao> Inscricao { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.ApplyConfiguration(new InscricaoMap());
+                modelBuilder.ApplyConfiguration(new UsuarioMap());
+
+                base.OnModelCreating(modelBuilder);
+            }
+
+        
+    }
+}
