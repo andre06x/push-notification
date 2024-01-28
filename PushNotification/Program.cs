6a6f70d05280f48c152ae206c2bba47edc2fb9bd
André Silva <andre06x@gmail.com>
2024-01-27 22:14:52 -0300

    Revert "add index App"
    This reverts commit f4ff23b4067b644b76952b12b4992e52aea3ee7f.

--- PushNotification/Program.cs
+++ PushNotification/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.FileProviders;
 using PushNotification.Configuracao;
 using WebPush;
-using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,14 +30,6 @@ builder.Services.AddDbContext<Contexto>(options =>
 );
 
 var app = builder.Build();
-app.UseStaticFiles(); // Este middleware serve arquivos estáticos do wwwroot
-
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = new PhysicalFileProvider(
-              Path.Combine(Directory.GetCurrentDirectory(), "App")),
-    RequestPath = "/App" 
-});
 
 if (app.Environment.IsDevelopment())
 {
